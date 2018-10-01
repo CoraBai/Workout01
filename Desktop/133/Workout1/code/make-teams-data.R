59bfc138db044f3790508e647cf5d49bec46bb21
Cora Bai <baiyt.cora2021@berkeley.edu>
2018-10-01 11:49:47 -0700

    upload make-teams-data.R

--- Desktop/133/Workout1/code/make-teams-data.R
+++ Desktop/133/Workout1/code/make-teams-data.R
@@ -23,9 +23,21 @@ missed_fg = dat$field_goals - dat$field_goals_atts
 missed_ft = dat$points1_atts - dat$points1
 rebounds = dat$off_rebounds + dat$def_rebounds
 efficiency = (dat$points + rebounds + dat$assists + dat$steals + dat$blocks + dat$blocks - missed_fg - missed_ft - dat$turnovers)/ dat$games 
-mutate(dat,missed_fg, missed_ft, rebounds, efficiency)
-
-
+dat = mutate(dat,missed_fg, missed_ft, rebounds, efficiency)
 sink("/Users/Cora/Desktop/133/Workout1/output/efficiency-summary.txt",append = TRUE)
 summary(efficiency)
+
+##Create data frame teams
+aggregate(dat$experience, by = list(team = dat$team), FUN = sum)
+new_df = aggregate(.~team, dat, sum)
+teams = new_df[ , c("team", "experience", "salary", "points3", "points2", "points", "off_rebounds", "def_rebounds",
+                  "assists", "steals", "blocks", "turnovers", "fouls", "efficiency")]
+sink("/Users/Cora/Desktop/133/Workout1/data/teams-summary.txt")
+summary(teams)
+write.csv(teams, file = "/Users/Cora/Desktop/133/Workout1/data/nba2018-teams.csv")
+
+
+
+
+
 
